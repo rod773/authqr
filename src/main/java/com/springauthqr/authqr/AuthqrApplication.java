1c638ff76b45c3eec3c74a7c5bb6d93905243274
rod773 <104315743+rod773@users.noreply.github.com>
2023-08-09 09:01:47 +0200

    initial commit

---
+++ src/main/java/com/springauthqr/authqr/AuthqrApplication.java
@@ -0,0 +1,13 @@
+package com.springauthqr.authqr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthqrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthqrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springauthqr/authqr/AuthqrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springauthqr.authqr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthqrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
